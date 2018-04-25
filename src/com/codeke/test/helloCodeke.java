acf6a1b94d2bd768401be558b17566c49fbcbe81
codeke <codeke@qq.com>
2018-04-25 09:20:36 +0800

    提价helloCodeke类

---
+++ src/com/codeke/test/helloCodeke.java
@@ -0,0 +1,13 @@
+package com.codeke.test;
+
+/**
+ * @company: 甘肃诚诚网络技术有限公司
+ * @author: codeke
+ * @date: 2018-04-25 9:19
+ * @description
+ */
+public class helloCodeke {
+    public static void main(String[] args) {
+        System.out.println("helloCodeke");
+    }
+}
