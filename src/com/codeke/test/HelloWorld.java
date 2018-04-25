20d274fd98ed1e0c95f7de710511a43df9238bea
codeke <125167016@qq.com>
2018-04-25 09:14:03 +0800

    添加第一个类

---
+++ src/com/codeke/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.codeke.test;
+
+/**
+ * @company: 甘肃诚诚网络技术有限公司
+ * @author: codeke
+ * @date: 2018/4/25 9:12
+ * @description
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
